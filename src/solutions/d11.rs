f1d766ad443fb49755ef1800eea1e8e85250848a
Deserteddd <eeauti@utu.fi>
2024-12-14 15:39:14 +0200

    more solutions

--- src/solutions/d11.rs
+++ src/solutions/d11.rs
@@ -1,36 +1,39 @@
+use cached::proc_macro::cached;
+
 pub fn d11() -> (u64, u64) {
-    let mut input = std::fs::read_to_string("./inputs/d11.txt").unwrap();
-    let mut input = "125 17".to_string();
-    let mut new = String::new();
-    (0..25).for_each(|n| {
-        // println!("Blink {}", n);
-        let now = std::time::Instant::now();
-            let mut start = 0;
-            let mut end = 0;
-            input.bytes().enumerate().for_each(|c| {
-                if c.1 == b' ' || c.0 == input.len()-1 {
-                    start = end;
-                    end = c.0;
-                    // println!("token at [{}..{}]: {}", start, end, &input[start..end])
-                }
-            });
-        let elapsed = now.elapsed();
-        input.clear();
-        input.push_str(&new);
-        new.clear();
-    });
-    let p1 = input.split_ascii_whitespace().count() as u64;
-    (p1, 0)
+    let input = std::fs::read_to_string("./inputs/d11.txt").unwrap();
+    // let input = "125 17".to_string();
+    let mut p1 = 0;
+    let mut p2 = 0;
+    let stones = input
+        .split_ascii_whitespace()
+        .map(|c| c.parse().unwrap())
+        .collect::<Vec<u64>>();
+
+    for stone in stones.iter().enumerate() {
+        p1 += blink(*stone.1, 0, 25);
+        p2 += blink(*stone.1, 0, 75);
+    }
+    (p1, p2)
 }
 
-fn update_stone(stone: &str) -> String{
-    if stone == "0" {
-        "1 ".to_string()
-    } else if stone.len() % 2 == 0 {
-        let first = &stone[..stone.len()/2];
-        let second = stone[stone.len()/2..].trim_start_matches('0');
-        format!("{} {} ", first, if second.is_empty() { "0 " } else { second })
+#[cached]
+fn blink(stone: u64, iter: u8, max_iter: u8) -> u64 {
+    if iter == max_iter {
+        return 1;
+    }
+    if stone == 0 {
+        return blink(1, iter + 1, max_iter);
+    }
+    let digits = stone.to_string();
+    let digit_count = digits.len();
+    if digit_count % 2 == 0 {
+        let mid = digit_count / 2;
+        let left = digits.get(..mid).unwrap().parse().unwrap();
+        let right = digits.get(mid..).unwrap().parse().unwrap();
+        return blink(left, iter + 1, max_iter) + blink(right, iter + 1, max_iter);
     } else {
-        format!("{} ", stone.parse::<u64>().unwrap()*2024)
+        return blink(stone * 2024, iter + 1, max_iter)
     }
 }
+
--- src/solutions/d9.rs
+++ src/solutions/d9.rs
@@ -1,3 +1,91 @@
+// 92092395920 is too low
 pub fn d9() -> (u64, u64) {
-    (0, 0)
+
+    let input = std::fs::read_to_string("./inputs/d9.txt").unwrap();
+    let input = "2333133121414131402";
+    // let input = "12345";
+    let decoded = decode(&input);
+    let part_1 = part_1(decoded.clone());
+    let part_2 = part_2(decoded);
+
+    (part_1, part_2) // too low
+}
+
+fn part_2(mut input: Vec<Option<u32>>) -> u64 {
+    println!("{:?}", input);
+    let empty = find_empty_block(&input, 3);
+    println!("Empty: {:?}", empty);
+    calculate_checksum(&input)
+}
+
+fn find_empty_block(disk: &Vec<Option<u32>>, size: usize) -> Option<usize> {
+    let mut count = 0;
+    let mut is_start = true;
+    let mut start = disk
+        .iter()
+        .enumerate()
+        .find(|(_, c)| c.is_none())
+        .unwrap().0;
+
+    for i in start..disk.len() {
+        if disk[i].is_none() {
+            if is_start {
+                is_start = false;
+                start = i
+            }
+            count += 1;
+            if count == size {
+                return Some(start)
+            }
+        } else {
+            count = 0;
+            is_start = true
+        }
+    }
+    None
+}
+
+fn part_1(mut input: Vec<Option<u32>>) -> u64 {
+    let mut empty_spaces = input
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|(_, c)| c.is_none())
+        .map(|(idx, _)| idx);
+
+    let mut rev_idx = input.len()-1;
+    while let Some(empty_idx) = empty_spaces.next() {
+        while input[rev_idx].is_none() && empty_idx < rev_idx {
+            rev_idx -= 1;
+        }
+        input[empty_idx] = input[rev_idx].take()
+    }
+    calculate_checksum(&input)
+}
+
+fn decode(input: &str) -> Vec<Option<u32>> {
+    let mut file = Vec::new();
+    let mut block_start = true;
+    let mut id: u32 = 0;
+    input.chars().for_each(|c| {
+        let num = c as u8 - b'0';
+        for _ in 0..num {
+            match block_start {
+                true => file.push(Some(id)),
+                false => file.push(None),
+            }
+        }
+        if block_start {id += 1};
+        block_start ^= true;
+    });
+    file
+}
+
+fn calculate_checksum(disk: &Vec<Option<u32>>) -> u64 {
+    disk
+        .iter()
+        .enumerate()
+        .filter(|elem| elem.1.is_some())
+        .map(|(idx, val)| idx as u64 * val.unwrap() as u64)
+        .sum()
 }
